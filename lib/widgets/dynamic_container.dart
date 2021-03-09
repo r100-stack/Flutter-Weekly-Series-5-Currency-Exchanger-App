22b794a6459c90eee3ec253318f4a6a5a5e9c0a1
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2021-03-08 22:10:40 -0600

    Added TODOs for step 6

--- lib/widgets/dynamic_container.dart
+++ lib/widgets/dynamic_container.dart
@@ -1,21 +1,25 @@
-import 'package:flutter/material.dart';
+// TODO (1): Create a stateless widget called DynamicContainer
+// TODO (2): Fields: Widget child, double maxWidth=800
 
-class DynamicContainer extends StatelessWidget {
-  final Widget child;
-  final double maxWidth;
+// TODO (3): return Container(alignment = Alignment.center)
+// TODO (4): Within this Container, add another Container with a maxWidth constraint and child
 
-  const DynamicContainer({this.child, this.maxWidth = 800});
+// class DynamicContainer extends StatelessWidget {
+//   final Widget child;
+//   final double maxWidth;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Container(
-        constraints: BoxConstraints(
-          maxWidth: maxWidth,
-        ),
-        child: child,
-      ),
-    );
-  }
-}
+//   const DynamicContainer({this.child, this.maxWidth = 800});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       alignment: Alignment.center,
+//       child: Container(
+//         constraints: BoxConstraints(
+//           maxWidth: maxWidth,
+//         ),
+//         child: child,
+//       ),
+//     );
+//   }
+// }
